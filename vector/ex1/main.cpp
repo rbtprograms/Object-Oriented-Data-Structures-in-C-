2687e38dd6bb337ef717ecc63c874f2fdfaf5427
Robert Thompson <rbtprograms@gmail.com>
2019-10-14 07:09:08 -0700

    vectors are statically typed dynamically growing arrays

---
+++ vector/ex1/main.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <iostream>
+
+int main() {
+  std::vector<int> v;
+  v.push_back(2);
+  v.push_back(3);
+  v.push_back(4);
+  v.push_back(5);
+  
+  std::cout << v[0] << std::endl;
+  std::cout << v[1] << std::endl;
+  std::cout << v[2] << std::endl;
+  std::cout << v[3] << std::endl;
+
+  return 0;
+}
