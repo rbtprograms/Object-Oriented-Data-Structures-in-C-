cb5f8bc02a7a6d497232e96cafeb3a01dd1a5fa7
Robert Thompson <rbtprograms@gmail.com>
2019-10-15 07:37:15 -0700

    wow solved it amaze

--- tower/Game.cpp
+++ tower/Game.cpp
@@ -6,12 +6,32 @@
 #include <iostream>
 using std::cout;
 using std::endl;
+using std::vector;
 
 void Game::solve() {
   // Prints out the state of the game:
   cout << *this << endl;
 
   // @TODO -- Finish solving the game!
+  move(4, stacks_[0], stacks_[2], stacks_[1]);
+}
+
+void Game::moveCube(Stack &s1, Stack &s2) {
+  Cube c = s1.removeTop();
+  s2.push_back(c);
+}
+
+void Game::move(int n, Stack &source, Stack &target, Stack &auxillary) {
+  if (n > 0) {
+    // move disks off of start and onto auxillary
+    move(n-1, source, auxillary, target);
+
+    //move cube
+    moveCube(source, target);
+
+    // move the cube off auxillary and onto our target
+    move(n-1, auxillary, target, source);
+  }
 }
 
 Game::Game() {
--- tower/Game.h
+++ tower/Game.h
@@ -12,4 +12,6 @@ class Game {
 
   private:
     std::vector<Stack> stacks_;
+    void moveCube(Stack & s1, Stack & s2);
+    void move(int n, Stack & source, Stack & target, Stack & spare);
 };
